1a6024cddb43a3c273ab020fb490884d37e12f25
Replit user <>
2023-04-23 20:19:38 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -57,7 +57,6 @@ void insertMap(HashMap * map, char * key, void * value)
 void enlarge(HashMap * map) 
 {
   enlarge_called = 1; //no borrar (testing purposes)
-  if(map==NULL) return;
   Pair **old_buckets = map->buckets;
   map->capacity *= 2;
   map->buckets = (Pair**) calloc(map->capacity, sizeof(Pair*))
